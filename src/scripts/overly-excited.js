e1325130e54eb87c9f00ac59bebb91e49bb4285a
rhc822 <rhc822@gmail.com>
2019-10-11 15:59:48 -0500

    Completed the exercise

--- src/scripts/overly-excited.js
+++ src/scripts/overly-excited.js
@@ -1,24 +1,63 @@
-// Create an array that contains the words in the sentence
-let sentence = ["The ","walrus ","danced ","through ","the ","trees ","in ","the ","light ","of ","the ","moon"];
+// // Practice: Stacking Words
 
-/*
-    The addExcitement function should be an impure function, and accept
-    the array as the sole argument. It should iterate over the array
-    and output the words to the browser console.
-*/
+// // Create an array that contains the words in the sentence
+// let sentence = ["The ","walrus ","danced ","through ","the ","trees ","in ","the ","light ","of ","the ","moon"];
+
+// /*
+//     The addExcitement function should be an impure function, and accept
+//     the array as the sole argument. It should iterate over the array
+//     and output the words to the browser console.
+// */
+// function addExcitement (theWordArray) {
+
+//     // Each time the for loop executes, you're going to add one more word to this string
+//     let buildMeUp = ""
+
+//     for (let i = 0; i < theWordArray.length; i++) {
+//         // Concatenate the new word onto buildMeUp
+//         buildMeUp = buildMeUp.concat(theWordArray[i]);
+//         // Print buildMeUp to the console
+//         console.log(buildMeUp);
+//     }
+
+// }
+
+// // Invoke the function and pass in the array
+// addExcitement(sentence)
+
+
+
+
+
+
+
+// Practice: Some words are more exciting than others
+
+let sentence = ["The ","walrus ","danced ","through ","the ","trees ","in ","the ","light ","of ","the ","moon "];
+
 function addExcitement (theWordArray) {
-
-    // Each time the for loop executes, you're going to add one more word to this string
     let buildMeUp = ""
 
     for (let i = 0; i < theWordArray.length; i++) {
-        // Concatenate the new word onto buildMeUp
-        buildMeUp = buildMeUp.concat(theWordArray[i]);
+        if ((i + 1) % 3 === 0){
+            buildMeUp = buildMeUp.concat(theWordArray[i] + "!");
+        /*
+            If the current word's place (not index) in the array
+            is evenly divisible by 3, add an exclamation point
+            to the end of the word and then concatenate it to
+            `buildMeUp`.
+
+            Otherwise, just concatenate the word itself.
+         */
+
+            } else {
+            buildMeUp = buildMeUp.concat(theWordArray[i]);
+            }
+
         // Print buildMeUp to the console
         console.log(buildMeUp);
     }
 
 }
 
-// Invoke the function and pass in the array
 addExcitement(sentence)
